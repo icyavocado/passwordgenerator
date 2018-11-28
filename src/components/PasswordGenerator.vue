9ff910180e3f57d451b2f66ff85793001debfd90
Icy Avocado <avocado@ovacoda.com>
2018-11-27 21:04:30 -0700

    UPDATE name of the file

--- src/components/PasswordGenerator.vue
+++ src/components/PasswordGenerator.vue
@@ -7,7 +7,13 @@ export default {
   name: "PasswordGenerator",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+          ascii_lowercase = 'abcdefghijklmnopqrstuvwxyz',
+    ascii_uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    digits = '0123456789',
+    hexdigits = '0123456789abcdefABCDEF',
+    octdigits = '01234567',
+      regexTest: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*?]{6,}$/,
+
     };
   }
 };
