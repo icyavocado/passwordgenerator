e15e0070fbefd5c002d188d132f0695d9f02bff6
Icy Avocado <avocado@ovacoda.com>
2018-11-27 20:58:01 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app"><PasswordGenerator /></div>
+</template>
+
+<script>
+import PasswordGenerator from "./components/PasswordGenerator";
+
+export default {
+  name: "App",
+  components: {
+    PasswordGenerator
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
